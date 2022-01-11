193042b3bb81a02d7679050bccbe5c71d70045cb
Bennett Perkins <bennettperkins@gmail.com>
2022-01-11 14:13:26 +0800

    Revert "al2022 image needs 6gb, not 4gb"
    This reverts commit ab1cefdb89e748ed144c2853efe43129a1ae269e.

--- src/lib/instance.ts
+++ src/lib/instance.ts
@@ -588,7 +588,7 @@ export class Instance extends pulumi.ComponentResource {
                           ),
                           rootBlockDevice: {
                               deleteOnTermination: true,
-                              volumeSize: args.rootVolumeSize ?? 6,
+                              volumeSize: args.rootVolumeSize ?? 4,
                               volumeType: 'gp3',
                               encrypted: true,
                               kmsKeyId,
