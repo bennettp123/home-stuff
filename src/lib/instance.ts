ba9d66f320fcf0e8f68053cc2e85b5bca47dd722
Bennett Perkins <bennettperkins@gmail.com>
2022-01-11 14:13:13 +0800

    Revert "al2022 needs yum-utils"
    This reverts commit c66166e3c434b9a9802af7189c942170a1cee9fb.

--- src/lib/instance.ts
+++ src/lib/instance.ts
@@ -65,11 +65,6 @@ fi
 `
 
 const upgradeAndReboot = `${upgrade}
-if which yum >/dev/null 2>&1; then
-  # yum-utils provides needs-restarting
-  yum install -y yum-utils
-fi
-
 # reboot if needed
 if which needs-restarting >/dev/null 2>&1; then
   cloud-init status --wait --long
