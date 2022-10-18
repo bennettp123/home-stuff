97210e05f24431d94fa56951b0cccd7039b47b01
Bennett Perkins <bennettperkins@gmail.com>
2022-10-18 11:00:42 +0800

    update default routes--bastions go direct instead of via openvpn

--- src/lib/default-routes.ts
+++ src/lib/default-routes.ts
@@ -41,12 +41,12 @@ export class DefaultRoutes extends pulumi.ComponentResource {
                         '192.168.0.0/18',
                         '192.168.128.0/18',
                         '192.168.192.0/18',
-                        '13.238.72.35/32', // jumpbox.cue-sandbox.swm.com.au
-                        '54.252.158.21/32', // bastion.swmdigital.io
-                        '52.63.148.231/32', // perthnow bastion
-                        '13.55.57.151/32', // branch-deploys bastion
-                        '54.79.218.38/32', // thewest bastion
-                        '3.104.86.9/32', // sevennews bastion
+                        //'13.238.72.35/32', // jumpbox.cue-sandbox.swm.com.au
+                        //'54.252.158.21/32', // bastion.swmdigital.io
+                        //'52.63.148.231/32', // perthnow bastion
+                        //'13.55.57.151/32', // branch-deploys bastion
+                        //'54.79.218.38/32', // thewest bastion
+                        //'3.104.86.9/32', // sevennews bastion
                     ].forEach((destinationCidrBlock, idx) =>
                         subnet.createRoute(
                             `home-${idx}`,
